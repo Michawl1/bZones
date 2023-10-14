8a71609064bcc573b8b7893b97c115d531e8ce63
Michael Thompson <mjt106@case.edu>
2023-10-14 12:56:01 -0400

    50 hz looks like the max speed to not starve lower priority tasks

--- main/bZoneStation.cpp
+++ main/bZoneStation.cpp
@@ -47,6 +47,6 @@ void NS::bZoneStation::run(
         {
             Serial.println("Train in: "+ String(pcTaskGetName(currTask)));
         }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(20 / portTICK_PERIOD_MS);
     }
 }
